bb5ad401ed207e883b54e60547a9f7ade6b73202
Mark McLoughlin <markbmc@gmail.com>
2024-03-30 16:42:02 +0000

    Retain metadata on resources

--- lib.py
+++ lib.py
@@ -2,6 +2,7 @@
 import os
 import os.path
 import urllib.parse
+import json
 from lxml import html
 
 URL_BASE = 'https://hyc.ie'
@@ -22,22 +23,40 @@ def login(session):
     return session.post(URL_BASE + "/login", data=payload)
 
 def get_resources_page(session, url):
-    resources = session.get(URL_BASE + url)
-    tree = html.fromstring(resources.text)
-    resources_hrefs = tree.xpath("//a[@class='inline_download_icon']/@href")
+    resources_text = session.get(URL_BASE + url).text
+    tree = html.fromstring(resources_text)
+    resources = []
+    for row in tree.xpath("//tr[@class='odd' or @class='even']"):
+        resource = {}
+        resource['url'] = row.xpath('.//td[2]/a/@href')[0]
+        resource['name'] = row.xpath('.//td[2]/a/text()')[0]
+        resource['filename'] = row.xpath('.//td[3]/text()')[0]
+        resource['mimetype'] = row.xpath('.//td[4]/text()')[0]
+        resource['date'] = row.xpath('.//td[5]/text()')[0]
+        resource['download_url'] = row.xpath('.//td[6]/a[contains(@class, "inline_download_icon")]/@href')[0]
+        resources.append(resource)
     next_hrefs = tree.xpath("//a[@rel='next']/@href")
-    return (resources_hrefs, next_hrefs[0] if len(next_hrefs) > 0 else None)
+    return (resources, next_hrefs[0] if len(next_hrefs) > 0 else None)
 
 def get_resources(session):
-    resources_hrefs, next_url = get_resources_page(session, "/admin/resources")
+    resources, next_url = get_resources_page(session, "/admin/resources")
     while next_url:
-        hrefs, next_url = get_resources_page(session, next_url)
-        resources_hrefs.extend(hrefs)
-    return resources_hrefs
+        next_resources, next_url = get_resources_page(session, next_url)
+        resources.extend(next_resources)
+    return resources
 
 def get_file(session, url, directory):
-    r = session.get(URL_BASE + url)
     filename = os.path.basename(url)
     if "?" in filename:
         filename = filename.split("?")[0]
-    open(os.path.join(directory, filename), 'wb').write(r.content)
+    path = os.path.join(directory, filename)
+    if not os.path.exists(path):
+        print(f"Downloading {path}")
+        r = session.get(URL_BASE + url)
+        open(path, 'wb').write(r.content)
+    else:
+        print(f"Skipping {path}")
+
+def write_metadata(objects, directory):
+    s = json.dumps(objects)
+    open(os.path.join(directory, 'metadata.json'), 'w').write(s)
